ae025690cca7353e0bce0cd7c4f1e0f7b0edd90d
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-06-27 16:01:39 -0700

    if applied, this commit adds variables to scripts to make it more maintainable

--- functions.php
+++ functions.php
@@ -2,17 +2,24 @@
 
 //Loads Custom Css
 function get_cst_styles(){
+    //Style Paths
+    $CUSTOM_CSS_PATH = '/resources/css/main.css';
+
     // Define the path to custom CSS file
-    $stylesheet_uri = get_template_directory_uri() . '/resources/css/main.css';
+    $stylesheet_uri = get_template_directory_uri() . $CUSTOM_CSS_PATH;
     // Enqueue the custom CSS file
     wp_enqueue_style('custom-style', $stylesheet_uri, array(), '1.0.0', 'all' );
 }
 
 //Loads Custom JavaScript
 function get_cst_scripts() {
+  //Script Paths
+  $FONT_AWESOME_PATH = '/node_modules/@fortawesome/fontawesome-free/js/all.js';
+  $CUSTOM_JS_PATH = '/resources/js/main.css';
+
   // Define the path to your custom JavaScript file
-  $font_awesome = get_template_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/js/all.js';
-  $script_uri = get_template_directory_uri() . '/resources/js/main.css';
+  $font_awesome = get_template_directory_uri() . $FONT_AWESOME_PATH;
+  $script_uri = get_template_directory_uri() . $CUSTOM_JS_PATH;
 
   // Enqueue the custom JavaScript file
   wp_enqueue_script( 'font-awesome', $font_awesome, array(), '1.0.0', 'all' );
@@ -21,7 +28,10 @@ function get_cst_scripts() {
 
 //pulls header.php from custom path
 function get_cst_header() {
-  $template_path = get_template_directory() . '/includes/header.php';
+  //Path
+  $HEADER_PATH = '/includes/header.php';
+
+  $template_path = get_template_directory() . $HEADER_PATH;
   if ( file_exists( $template_path ) ) {
     include( $template_path );
   }
@@ -29,7 +39,10 @@ function get_cst_header() {
 
 //pulls footer.php from custom path
 function get_cst_footer() {
-  $template_path = get_template_directory() . '/includes/footer.php';
+   //Path
+  $FOOTER_PATH = '/includes/footer.php';
+
+  $template_path = get_template_directory() . $FOOTER_PATH; 
   if ( file_exists( $template_path ) ) {
     include( $template_path );
   }
