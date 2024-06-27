b3e52345ee3626e22aa73aec8db7ec8c52e6a740
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-06-27 15:48:56 -0700

    if applied, this commit corrects problems with font awesome import and connects custom js with head.php

--- functions.php
+++ functions.php
@@ -1,10 +1,22 @@
 <?php 
 
+//Loads Custom Css
 function get_cst_styles(){
     // Define the path to custom CSS file
     $stylesheet_uri = get_template_directory_uri() . '/resources/css/main.css';
     // Enqueue the custom CSS file
     wp_enqueue_style('custom-style', $stylesheet_uri, array(), '1.0.0', 'all' );
+}
+
+//Loads Custom JavaScript
+function get_cst_scripts() {
+  // Define the path to your custom JavaScript file
+  $font_awesome = get_template_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/js/all.js';
+  $script_uri = get_template_directory_uri() . '/resources/js/main.css';
+
+  // Enqueue the custom JavaScript file
+  wp_enqueue_script( 'font-awesome', $font_awesome, array(), '1.0.0', 'all' );
+  wp_enqueue_script( 'custom-script', $script_uri, array(), '1.0.0', 'all' );
 }
 
 //pulls header.php from custom path
@@ -24,5 +36,6 @@ function get_cst_footer() {
 }
 
 add_action('wp_enqueue_scripts', 'get_cst_styles');
+add_action('wp_enqueue_scripts', 'get_cst_scripts');
 
 
